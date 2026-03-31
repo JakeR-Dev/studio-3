9adabc1eeb5abb14d0a2b7e431aec248a053059f
Jake Ryan <jryan6492@gmail.com>
2026-03-31 14:21:40 -0400

    update vite base url for GH pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: './',
+  base: '/studio-3/',
   build: {
     cssMinify: 'esbuild',
   },
